f6f80a7639fc5d8db0ccad2f69ead0195171f340
prashansa-git <pgopi@student.gitam.edu>
2024-11-27 08:50:13 +0530

    Create armstrong-number

---
+++ armstrong-number.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num, originalNum, remainder, result = 0, n = 0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    // Count the number of digits
+    while (originalNum != 0) {
+        originalNum /= 10;
+        ++n;
+    }
+
+    originalNum = num;
+
+    // Calculate the sum of the cubes of individual digits
+    while (originalNum != 0) {
+        remainder = originalNum % 10;
+        result += pow(remainder, n);
+        originalNum /= 10;
+    }
+
+    // Check if the result is equal to the original number
+    if (result == num)
+        printf("%d is an Armstrong number.\n", num);
+    else
+        printf("%d is not an Armstrong number.\n", num);
+
+    return 0;
+}
+
+output
+Enter an integer: 4
+4 is an Armstrong number.
